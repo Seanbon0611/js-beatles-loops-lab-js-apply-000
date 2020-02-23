6115423a5b060ef6bf7cab7a4393892daf3ee739
Sean Dever <seand0611@gmail.com>
2020-02-23 05:18:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// add solution here
+function theBeatlesPlay (musicians, instruments) {
+  let theBeatles = []
+  for (let i=0; i < musicians.length; i++) {
+    theBeatles.push(musicians[i] + ' plays ' + instruments[i])
+  }
+  return theBeatles;
+}
+
+function johnLennonFacts() {
+  const facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+  let updatedFacts = [];
+  let i = 0;
+  while (i < facts.length) {
+    facts + "!" + i
+  }
+  return facts
+}
